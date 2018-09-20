692aee92be25a903cabc0201a7fc98604212de24
Mike Choi <mike.choi@live.ca>
2018-09-20 00:36:02 -0700

    Added all the regex and refactored code to only have one validation function

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 // Blur event listeners
-document.getElementById('name').addEventListener('blur', validateName);
-document.getElementById('postal').addEventListener('blur', validatePostal);
-document.getElementById('email').addEventListener('blur', validateEmail);
-document.getElementById('phone').addEventListener('blur', validatePhone);
+document.getElementById('name').addEventListener('blur', validate);
+document.getElementById('postal').addEventListener('blur', validate);
+document.getElementById('email').addEventListener('blur', validate);
+document.getElementById('phone').addEventListener('blur', validate);
 
-function validateName(e){
-    const name = e.target.value;
-    const pattern = /^[a-zA-Z]{2,10}$/;
-    if(!pattern.test(name) && !e.target.classList.contains('is-invalid')){
+function validate(e){
+    const input = e.target.value;
+    let pattern;
+
+    if(e.target.id === 'name'){
+        pattern = /^[a-z]{2,10}$/i;
+    } else if(e.target.id === 'postal'){
+        pattern = /^(?!.*[DFIOQU])[A-VXY]\d[A-Z] ?[0-9][A-Z][0-9]$/i;
+    } else if(e.target.id === 'email'){
+        pattern = /^(?!\.)(?!.*\.{2})([a-zA-Z0-9!#$%&'*-+/=?^_`.{|}~]){1,64}@([a-zA-Z0-9\-]{1,63}\.){1,3}([a-z]{2,3})$/;
+    } else if(e.target.id === 'phone'){
+        pattern = /^(\d{3} ?){2}\d{4}$/;
+    }
+
+    if(!pattern.test(input) && !e.target.classList.contains('is-invalid')){
         e.target.classList.add('is-invalid');
-    } else if(pattern.test(name) && e.target.classList.contains('is-invalid')){
+    } else if(pattern.test(input) && e.target.classList.contains('is-invalid')){
         e.target.classList.remove('is-invalid')
     }
-}
-
-function validatePostal(e){
-
-}
-
-function validateEmail(e){
-
-}
-
-function validatePhone(e){
-
 }
