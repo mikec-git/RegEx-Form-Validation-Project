c42f46de8b6c903ac64bbae01e08b504460ced1b
Mike Choi <mike.choi@live.ca>
2018-09-20 00:39:02 -0700

    Modified regex pattern for email

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function validate(e){
     } else if(e.target.id === 'postal'){
         pattern = /^(?!.*[DFIOQU])[A-VXY]\d[A-Z] ?[0-9][A-Z][0-9]$/i;
     } else if(e.target.id === 'email'){
-        pattern = /^(?!\.)(?!.*\.{2})([a-zA-Z0-9!#$%&'*-+/=?^_`.{|}~]){1,64}@([a-zA-Z0-9\-]{1,63}\.){1,3}([a-z]{2,3})$/;
+        pattern = /^(?!\.)(?!.*\.{2})([a-zA-Z0-9!#$%&'*-+/=?^_`.{|}~]){1,64}@([a-zA-Z0-9\-]{1,63}\.){1,3}([a-zA-Z]{2,5})$/;
     } else if(e.target.id === 'phone'){
         pattern = /^(\d{3} ?){2}\d{4}$/;
     }
